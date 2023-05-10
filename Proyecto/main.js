0f357c39961a32179cd3b86ef0f514cbd9e675cc
tomi lago <tomilago1@gmail.com>
2023-05-09 21:38:14 -0300

    realizando ciclo For

--- Proyecto/main.js
+++ Proyecto/main.js
@@ -19,7 +19,6 @@ switch(cuotas){
         alert("Realizaras la compra en doce cuotas de $"+ (valor + interes) /12)
         alert(mensajeInteres)
     break
-
     default: 
     alert("La cantidad de cuotas ingresada no es valida")
     break
@@ -32,6 +31,25 @@ switch(cuotas){
      }
  }
 
+// for(let i=0; i <=24; i++){
+//     if(i===3 ||i===6 || i===12|| i===18|| i===24){
+//         console.log("Cantidad de cuotas disponible")
+//     }else{
+//         console.log("Cantidad de cuotas no disponible")
+//     }
+// }
 
 
+
+let nombreProducto = prompt("Ingrese el nombre de su producto")
+let productosAComprar = parseInt(prompt("Ingrese numero de productos a comprar"))
+let precioProducto = parseInt(prompt("Ingrese el precio del producto que quiere comprar"))
+
+console.log("Dia de decuentos")
+for(let i=0; i<=productosAComprar ; i++){
+    let resultadoDto = precioProducto *0.20
+    let descuento = precioProducto - resultadoDto
+    console.log(`obtuviste un descuento del 20% en ${nombreProducto} tu dinero a abonar es de ${descuento}`)
+
+}
      
