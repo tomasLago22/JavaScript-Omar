6083c4254b7c2db6ac7b335f15b371e6c5280662
tomi lago <tomilago1@gmail.com>
2023-05-09 23:54:27 -0300

    correcion de errores

--- Proyecto/main.js
+++ Proyecto/main.js
@@ -8,14 +8,17 @@ let mensajeInteres = "Debido al interes aplicado el valor de tu producto es de $
 
 switch(cuotas){
     case "tres":
+    case "3":
     alert("Realizaras la compra en tres cuotas de $"+ valor /3)
     break
 
     case "seis":
+    case "6":
         alert("Realizaras la compra en seis cuotas de $"+ valor /6)
     break
 
     case "doce":
+    case "12":
         alert("Realizaras la compra en doce cuotas de $"+ (valor + interes) /12)
         alert(mensajeInteres)
     break
@@ -26,18 +29,12 @@ switch(cuotas){
 }
 
  function calcularInteres (cuotas, valor){
-     if (cuotas == "doce"){
+     if (cuotas == "doce" || cuotas == "12"){
          return valor * 0.2 
      }
  }
 
-// for(let i=0; i <=24; i++){
-//     if(i===3 ||i===6 || i===12|| i===18|| i===24){
-//         console.log("Cantidad de cuotas disponible")
-//     }else{
-//         console.log("Cantidad de cuotas no disponible")
-//     }
-// }
+
 
 
 
@@ -49,7 +46,7 @@ console.log("Dia de decuentos")
 for(let i=0; i<=productosAComprar ; i++){
     let resultadoDto = precioProducto *0.20
     let descuento = precioProducto - resultadoDto
-    console.log(`obtuviste un descuento del 20% en ${nombreProducto} tu dinero a abonar es de ${descuento}`)
+    alert(`obtuviste un descuento del 20% en ${nombreProducto} tu dinero a abonar es de ${descuento}`)
 
 }
      
