78884cd9706a0615f0631fd78cc722c7e765683a
tomi lago <tomilago1@gmail.com>
2023-06-12 22:09:55 -0300

    3era entrega

--- Proyecto/main.js
+++ Proyecto/main.js
@@ -1,74 +1,14 @@
-let registrarEmail = prompt("Ingrese su Email");
-
-let registrarContrasenia = prompt("Ingrese su contraseña");
-
-let confirmarContrasenia = prompt("Ingrese nuevamente su contraseña");
-
-while (registrarContrasenia.length < 6) {
-    alert("La contraseña debe tener al menos 6 caracteres. Inténtelo nuevamente.");
-    registrarContrasenia = prompt("Ingrese su contraseña:");
-}
-
-while (registrarContrasenia !== confirmarContrasenia) {
-    alert("Sus contraseñas no coinciden. Inténtelo nuevamente.");
-    confirmarContrasenia = prompt("Confirme nuevamente su contraseña");
-}
-
-alert("Registro Exitoso!")
-
- let valor = Number(prompt("Ingrese el valor de su producto"));
-
-let cuotas = prompt("Seleccione en cuantas cuotas pagara el producto").toLowerCase();
-
-let interes = calcularInteres(cuotas,valor)
-
-let mensajeInteres = "Debido al interes aplicado el valor de tu producto es de $" + (valor + interes) 
-
-switch(cuotas){
-    case "tres":
-    case "3":
-    alert("Realizaras la compra en tres cuotas de $"+ valor /3)
-    break
-
-    case "seis":
-    case "6":
-        alert("Realizaras la compra en seis cuotas de $"+ valor /6)
-    break
-
-    case "doce":
-    case "12":
-        alert("Realizaras la compra en doce cuotas de $"+ (valor + interes) /12)
-        alert(mensajeInteres)
-    break
-    default: 
-    alert("La cantidad de cuotas ingresada no es valida")
-    break
-    
-}
-
- function calcularInteres (cuotas, valor){
-     if (cuotas == "doce" || cuotas == "12"){
-         return valor * 0.2 
-     }
- }
+document.addEventListener("DOMContentLoaded", function() {
 
 
 
+//Get item
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-let nombreProducto = prompt("Ingrese el nombre de su producto")
-let productosAComprar = parseInt(prompt("Cuantas compras vas a realizar en el dia de hoy?"))
-let precioProducto = parseInt(prompt("Ingrese el precio del producto que quiere comprar"))
+const carritoContenedor = document.querySelector("#carritoContenedor")
 
-console.log("Dia de decuentos")
-for(let i=0; i <= productosAComprar ; i++){
-    let resultadoDto = precioProducto  *0.20
-    let descuento = precioProducto - resultadoDto
-    alert(`obtuviste un descuento del 20% en ${nombreProducto} tu dinero a abonar es de ${descuento}`)
-
-}
-      
 const productos = [
-  {id: 1, nombre: "Remera St Marie Prisoner", precio: 10000},
+  { id: 1, nombre: "Remera St Marie Prisoner", precio: 10000 },
   { id: 2, nombre: "Pantalon Waffle", precio: 15000 },
   { id: 3, nombre: "Camisa XXL", precio: 12000 },
   { id: 4, nombre: "Swater Polera Animal Print", precio: 11000 },
@@ -76,46 +16,111 @@ const productos = [
 ];
 
 class Producto {
-    constructor(id, nombre, precio) {
-      this.id = id;
-      this.nombre = nombre;
-      this.precio = precio;
-    }
+  constructor(id, nombre, precio) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
   }
-  
-  
-  const producto6 = new Producto(6, "Campera Oversize Matelasse St Marie", 17000);
-  productos.push(producto6)
-  
-  const botonAgregarCarrito = document.querySelectorAll('.botonAgregarCarrito');
+}
 
-  botonAgregarCarrito.forEach(boton => {
-    boton.addEventListener('click', function() {
-      const idProducto = parseInt(this.id.replace('agregar', ''));
+const producto6 = new Producto(6, "Campera Oversize Matelasse St Marie", 17000);
+productos.push(producto6);
 
+// Creando estilos en DOM
+const divProducto = document.querySelector(".contenedorProductos");
+
+const productosHTML = productos.map(producto => {
+  return `
+    <div class="producto">
+      <h4>${producto.nombre}</h4>
+      <p>${producto.precio}</p>
+      <button id="agregar${producto.id}" class="botonAgregarCarrito">Agregar al Carrito</button> 
+    </div>`;
+});
+
+divProducto.innerHTML = productosHTML.join("");
+
+const botonAgregarCarrito = document.querySelectorAll('.botonAgregarCarrito');
+
+botonAgregarCarrito.forEach(boton => {
+  boton.addEventListener('click', function() {
+    const idProducto = parseInt(this.id.replace('agregar', ''));
     const producto = productos.find(item => item.id === idProducto);
-    
+
     if (producto) {
       carrito.push(producto);
+      Toastify({
+        text: `Agregaste ${producto.nombre} al Carrito!`,
+        duration: 3000,
+        destination: "https://github.com/apvarun/toastify-js",
+        avatar: "https://cdn-icons-png.flaticon.com/512/107/107831.png?w=826&t=st=1686186351~exp=1686186951~hmac=558bf283326ec3388f4efa85674f5895c493073817746060a181d1d953d44631",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function() {} // Callback after click
+      }).showToast();
       actualizarCarrito();
+      guardarLocal();
     }
-    });
   });
-  
-  const carrito = []
+});
 
+const buscar = document.querySelector("#buscar");
 
-  /* 
-    const agregarUno = document.querySelector("#agregarUno");
-    
-    agregarUno.addEventListener('click', function(){
-      carrito.push(producto1);
-  }); */
-  
-  function actualizarCarrito(){
+buscar.addEventListener("input", () => {
+  const terminoBusqueda = buscar.value.toLowerCase();
+  filtrarProductos(terminoBusqueda);
+});
+
+function filtrarProductos(termino) {
+  const elementosProductos = document.querySelectorAll(".producto");
+
+  elementosProductos.forEach(elemento => {
+    const nombreProducto = elemento.querySelector("h4").textContent.toLowerCase();
+
+    if (nombreProducto.includes(termino)) {
+      elemento.classList.remove('no-mostrar');
+    } else {
+      elemento.classList.add('no-mostrar');
+ 
+    }
+  });
+}
+
+function actualizarCarrito() {
   let totalCompra = carrito.reduce((accum, prod) => {
-     return accum + prod.precio;
-    }, 0);
+    return accum + prod.precio;
+  }, 0);
   
-  console.log(`El total del valor agregado a su Carrito es de ${totalCompra}`);
-  }
+  console.log(`El Valor Total de su Carrito es de ${totalCompra}`)
+
+/*   carrito.forEach((producto)=>{
+    const div = document.createElement("div")
+    div.className = ("pruductosCarrito")
+    div.innerHTML `
+    
+    <h4>${producto.nombre}</h4>
+      <p>${producto.precio}</p>
+      <p> Cantidad: <span id= "cantidad"> ${producto.cantidad}</span> </p>
+      <button onclick = "eliminarDelCarrito(${producto.id})" class = "botonEliminar"><i class="fas fa-trash-alt"</button>
+    `
+    carritoContenedor.appendChild(div)
+
+  }) */
+
+
+}
+
+//Set item
+const guardarLocal = () => {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+
+
+});
